22acadd7a589c7dd1cc31738d1c4c0aa15da36c2
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-03-09 16:15:57 +0200

    Send object model to view

---
+++ asp_net_mvc_spd221/Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_mvc_spd221.Controllers
+{
+    public class ContactsController : Controller
+    {
+        List<Contact> contacts;
+        public ContactsController()
+        {
+            contacts = new List<Contact>()
+            {
+                new Contact() { Id = 1, Name = "Olga", Phone = "33-445-434" },
+                new Contact() { Id = 2, Name = "Igor", Phone = "23-23-23" },
+                new Contact() { Id = 3, Name = "Sofia", Phone = "999-66-99" }
+            };
+        }
+        public IActionResult Index()
+        {
+            // load data from db...
+            return View(contacts);
+        }
+    }
+}
---
+++ asp_net_mvc_spd221/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace asp_net_mvc_spd221.Controllers
+{
+    internal class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
