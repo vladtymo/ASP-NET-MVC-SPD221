c1b1be1a4793c73659aba30eafd5ae77c9639a18
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-03-09 14:58:51 +0200

    Add project files.

---
+++ asp_net_mvc_spd221/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using asp_net_mvc_spd221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace asp_net_mvc_spd221.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View(); // ~/Views/Home/Index.cshtml
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
