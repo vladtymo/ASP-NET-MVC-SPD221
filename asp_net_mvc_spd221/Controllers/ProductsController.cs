408602212d98c7835ff46dd0e6e9a4f85325d332
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-03-16 14:57:43 +0200

    Create admin product page.

---
+++ asp_net_mvc_spd221/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using asp_net_mvc_spd221.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_mvc_spd221.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ShopDbContext context;
+
+        public ProductsController()
+        {
+            context = new ShopDbContext();
+        }
+
+        public IActionResult Index()
+        {
+            // get products from db
+            var products = context.Products.ToList();
+
+            return View(products);
+        }
+
+        // create/edit/delete/find
+    }
+}
