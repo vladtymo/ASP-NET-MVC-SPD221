660d061de6cc30bb69a9a7cbd517de742f5c7477
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-03-23 15:39:13 +0200

    Add category select list

--- asp_net_mvc_spd221/Controllers/ProductsController.cs
+++ asp_net_mvc_spd221/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
 ﻿using asp_net_mvc_spd221.Data;
 using asp_net_mvc_spd221.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace asp_net_mvc_spd221.Controllers
 {
@@ -17,7 +19,8 @@ namespace asp_net_mvc_spd221.Controllers
         public IActionResult Index()
         {
             // get products from db
-            var products = context.Products.ToList();
+            // .Include() - LEFT JOIN
+            var products = context.Products.Include(x => x.Category).ToList();
 
             return View(products);
         }
@@ -25,12 +28,19 @@ namespace asp_net_mvc_spd221.Controllers
         [HttpGet] // by default
         public IActionResult Create()
         {
+            // ViewBag - transfer data from action to view
+
+            var categories = new SelectList(context.Categories.ToList(), nameof(Product.Id), nameof(Product.Name));
+            ViewBag.Categories = categories;
+
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(Product model)
         {
+            // TODO: add model validation
+
             context.Products.Add(model);
             context.SaveChanges();
 
